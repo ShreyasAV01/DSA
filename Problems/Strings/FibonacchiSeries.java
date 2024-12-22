e3e0a5c8b0c6084e58d7369d0e1d94c3a6306d0a
Shreyas <57635352+ShreyasAV01@users.noreply.github.com>
2024-12-22 09:17:03 +0530

    FibonacchiSeries using recursion

--- Problems/Strings/FibonacchiSeries.java
+++ Problems/Strings/FibonacchiSeries.java
@@ -1,7 +1,9 @@
 package Strings;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FibonacchiSeries {
 
@@ -18,8 +20,35 @@ public class FibonacchiSeries {
         }
         System.out.println("Fibonacci Series is : "+fibonacchiIntList);
     }
+
+    private static Integer getFibonacciUsingRecursion(Integer limit){
+        Map<Integer,Integer> integerMap = new HashMap<>();
+
+        if (integerMap.containsKey(limit)){
+            return integerMap.get(limit);
+        }
+
+        if (limit ==0 ) return 0;
+        if (limit ==1 ) return 1;
+
+        int result = getFibonacciUsingRecursion(limit - 1) + getFibonacciUsingRecursion(limit - 2);
+        integerMap.put(limit,result);
+        return result;
+    }
+
+    private static void printFibonacciSeriesUsingRecursion(Integer limit){
+        List<Integer> integerList = new ArrayList<>();
+        for (int i =0; i<limit; i++){
+            Integer result = getFibonacciUsingRecursion(i);
+            integerList.add(result);
+        }
+        System.out.print("Fibonacci Series using recursion is : "+integerList);
+    }
+
+
     public static void main(String[] args) {
         getFibonacciUsingDynamicProgramming(10);
+        printFibonacciSeriesUsingRecursion(10);
 
     }
 
