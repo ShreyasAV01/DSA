562fcf6359f898741eee0d3cb68e390ca5b8591c
Shreyas <57635352+ShreyasAV01@users.noreply.github.com>
2025-11-19 23:47:40 +0530

    Second highest value in a arraylist

--- Problems/Streams/SecondHighestNumberInArray.java
+++ Problems/Streams/SecondHighestNumberInArray.java
@@ -8,11 +8,13 @@ import java.util.List;
 public class SecondHighestNumberInArray {
 
 
-
     public static void main(String[] args) {
-        List<Integer> integerList =  Arrays.asList(1,2,4,32,5,78,3,6,5,8,9);
+        List<Integer> integerList = Arrays.asList(1, 2, 4, 32, 5, 78, 3, 6, 5, 8, 9);
 
-        Integer integer = integerList.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
+        Integer integer = integerList.stream().sorted(Comparator.reverseOrder()).
+                skip(1).
+                findFirst().
+                get();
 
         System.out.print("Second largest value is "+integer);
 
