a1cd542d9161de3ef770d0566b4286b756e68de6
Shreyas <57635352+ShreyasAV01@users.noreply.github.com>
2025-11-20 20:29:32 +0530

    Find Duplicates Along With Frequency using Java 8

---
+++ Problems/Streams/FindDuplicatesAlongWithFrequency.java
@@ -0,0 +1,24 @@
+package Streams;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindDuplicatesAlongWithFrequency {
+
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 78, 9, 1, 2, 5, 33, 3, 3, 5, 7);
+        HashMap<Integer, Integer> freqHashmap = new HashMap<>();
+
+        for (Integer  i:  integerList){
+            freqHashmap.put(i, freqHashmap.getOrDefault(i,0) + 1);
+        }
+
+        for (Map.Entry<Integer,Integer> entry: freqHashmap.entrySet()) {
+            System.out.println("Element "+ entry.getKey() + " Frequency "+entry.getValue());
+        }
+
+
+    }
+}
