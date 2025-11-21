c66b90bb14c9129336f7dea428f8c628fe4c9274
Shreyas <57635352+ShreyasAV01@users.noreply.github.com>
2025-11-21 07:25:36 +0530

    refactored

--- Problems/Streams/FindFrequencyOfOccurrence.java
+++ Problems/Streams/FindFrequencyOfOccurrence.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class FindDuplicatesAlongWithFrequency {
+public class FindFrequencyOfOccurrence {
 
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 78, 9, 1, 2, 5, 33, 3, 3, 5, 7);
