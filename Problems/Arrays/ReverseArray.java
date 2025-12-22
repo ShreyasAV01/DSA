6393cee9b8eb6fd0e89a231cb58d776a81e9e962
Shreyas <57635352+ShreyasAV01@users.noreply.github.com>
2025-12-22 10:01:44 +0530

    reverse array

--- Problems/Arrays/ReverseArray.java
+++ Problems/Arrays/ReverseArray.java
@@ -73,6 +73,8 @@ public class ReverseArray {
         }
         System.out.println();
     }
+
+
     public static void main(String[] args){
         int[] arr = {1, 2, 3, 4, 5, 6};
         int[] arr1 = {1, 2, 3, 4, 5, 6};
