bb610249e47750949eec80801613525835aac070
Shreyas <57635352+ShreyasAV01@users.noreply.github.com>
2025-12-22 08:44:13 +0530

    Third largest element in array

---
+++ Problems/Arrays/ThirdLargestElement.java
@@ -0,0 +1,69 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ThirdLargestElement {
+
+    public static int thirdLargestUsingSorting (int [] arr){
+        int thirdLargestElement;
+        Arrays.sort(arr);
+        thirdLargestElement = arr[arr.length-3];
+        System.out.println("Third largest element using thirdLargestUsingSorting is : "+ thirdLargestElement);
+        return thirdLargestElement;
+    }
+
+    public static int thirdLargestUsingThreeLoops(int [] arr) {
+        int n = arr.length;
+
+        // Find the first maximum element.
+        int first = Integer.MIN_VALUE;
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i] > first)
+                first = arr[i];
+        }
+//        Find the second largest element
+        int second = Integer.MIN_VALUE;
+        for (int i =0 ; i< arr.length; i++){
+            if (arr[i] > second  && arr[i] < first)
+                second = arr[i];
+        }
+
+//        Find the third largest element
+        int third = Integer.MIN_VALUE;
+        for (int i =0; i< arr.length ; i++){
+            if (arr[i] > third && arr[i]< second)
+                third = arr[i];
+        }
+
+        // Return the third largest element
+        System.out.println("Third largest element using thirdLargestUsingThreeLoops is : "+ third);
+        return third;
+
+    }
+
+    public static int thirdLargestUsingSingleLoop(int[] arr){
+        int firstLargest = Integer.MIN_VALUE , secondLargest =  Integer.MIN_VALUE, thirdLargest =  Integer.MIN_VALUE;
+
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] > firstLargest){
+                thirdLargest = secondLargest;
+                secondLargest = firstLargest;
+                firstLargest = arr[i];
+            } else if (arr[i] >secondLargest){
+                thirdLargest = secondLargest;
+                secondLargest = arr[i];
+            } else if (arr[i] > thirdLargest) {
+                thirdLargest = arr[i];
+            }
+        }
+        System.out.println("Third largest element using thirdLargestUsingSingleLoop is : "+ thirdLargest);
+        return thirdLargest;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 14, 2, 16, 10, 20};
+        thirdLargestUsingSorting(arr);
+        thirdLargestUsingThreeLoops(arr);
+        thirdLargestUsingSingleLoop(arr);
+    }
+    
+}
